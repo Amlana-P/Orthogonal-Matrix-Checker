9eead505ea0fc8552bfc91a0117d456ad969c03d
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2021-06-20 13:34:09 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700">
-    
     <title>Orthogonal Matrix Checker</title>
 </head>
 <body>
+  <h1>Orthogonal Matrix Checker</h1>
+  <h3>Ashutosh Samal 118EE707</h3>
   <form class="form" action="action_page.php" method="POST">
     <input class="form_input" type="number" name="rows" placeholder="Input number of rows" min="1" required>
     <input class="form_input" type="number" name="columns" placeholder="Input number of columns" min="1" required>
     <input class="form_submit" type="submit" name="submit"></input>
   </form>
-<script src="script.js"></script>
 </body>
 </html>
